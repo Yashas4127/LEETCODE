ad1b41c99c256e4589a7dfe732c5cbe8b7db41ef
YASHAS  H M <yashashm19@gmail.com>
2026-07-06 07:29:46 +0530

    Time: 7 ms (27.3%), Space: 18.4 MB (64.84%) - LeetHub

--- LeetCode/Hard/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ LeetCode/Hard/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,35 +10,34 @@
  */
 class Solution {
 public:
-    struct cmp{
-        bool operator()(ListNode* first,ListNode* second){
-            return first->val>second->val;
+    struct cmp {
+        bool operator()(ListNode* first, ListNode* second) {
+            return first->val > second->val;
         }
     };
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,cmp>minHeap;
 
-        for(int i=0;i<lists.size();i++){
-            if(lists[i]){
-                minHeap.push(lists[i]);
-            }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> minHeap;
+
+        for (ListNode* head : lists) {
+            if (head)
+                minHeap.push(head);
         }
 
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
 
-        ListNode* dummy=new ListNode(-1);
-        ListNode* temp=dummy;
-
-        while(!minHeap.empty()){
-            ListNode* curr=minHeap.top();
+        while (!minHeap.empty()) {
+            ListNode* curr = minHeap.top();
             minHeap.pop();
 
-            temp->next=curr;
-            temp=curr;
-            curr=curr->next;
-            if(curr){
-                minHeap.push(curr);
-            }
+            temp->next = curr;
+            temp = curr;
+
+            if (curr->next)
+                minHeap.push(curr->next);
         }
+
         return dummy->next;
     }
 };

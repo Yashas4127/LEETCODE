8155b382ed1ca8c0c467e714c26d99ec5a6b5f12
YASHAS  H M <yashashm19@gmail.com>
2026-07-13 15:19:54 +0530

    Time: 4 ms (22.7%), Space: 52.2 MB (83.62%) - LeetHub

--- LeetCode/Medium/0055-jump-game/0055-jump-game.cpp
+++ LeetCode/Medium/0055-jump-game/0055-jump-game.cpp
@@ -3,6 +3,18 @@ public:
     bool canJump(vector<int>& nums) {
         int maxR=0;
         for(int i=0;i<nums.size();i++){
+
+            //2 3 1 1 4
+            //0 1 2 3 4
+            //maxR=2 4 3 4 8 true
+
+
+            //3 2 1 0 4
+            //0 1 2 3 4
+            //maxR=3 3 3 3 8  but here 4(next index)>3 false
+
+            //if index greater than maxR it means 
+            //maxR=3 ,index=5 than it can never reach there
             if(i>maxR) return false;
             maxR=max(maxR,i+nums[i]);
         }
